423a280f3599a9c594d9428b43ff9fe4affca015
skwidhubz <tommaxwellparker@gmail.com>
2023-01-09 19:44:37 +1100

    first js edit

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
 
 
 
+
+
+
+
+// SEARCH HISTORY FUNCTION (HISTORY PAGE)((LOCAL STORAGE))
+
+// function searchHistorySave(){
+//     save each user search "location name" --> saves to localStorage
+// }
+
