ee10d73022de7f3775e34c4fbd2c911cefc5ad23
byunn90 <kayhanturk90@Live.com>
2023-01-09 20:46:04 +1100

    added API

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,20 @@
+const encodedParams = new URLSearchParams();
+encodedParams.append("q", "Hello, world!");
+encodedParams.append("target", "es");
+encodedParams.append("source", "en");
 
+const options = {
+  method: "POST",
+  headers: {
+    "content-type": "application/x-www-form-urlencoded",
+    "Accept-Encoding": "application/gzip",
+    "X-RapidAPI-Key": "7e93e346d2mshbf8a71decc7eeb0p104a79jsn20b4da9a402e",
+    "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+  },
+  body: encodedParams,
+};
 
-
-
-
-
-
-// SEARCH HISTORY FUNCTION (HISTORY PAGE)((LOCAL STORAGE))
-
-// function searchHistorySave(){
-//     save each user search "location name" --> saves to localStorage
-// }
-
+fetch("https://google-translate1.p.rapidapi.com/language/translate/v2", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
