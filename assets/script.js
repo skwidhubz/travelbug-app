5f7dec7a8d9b77440f00ec50e7280071e9f267d0
byunn90 <kayhanturk90@Live.com>
2023-01-09 22:42:27 +1100

    finished buttons and added functions for buttons

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,24 @@
+// Variables for buttons
+const btnSubmit = document.getElementById("btn-control");
+const btnLocate = document.getElementById("btn-locate");
+let text = document.getElementById("text-box");
 
+// Kayhan Objective Function
+function objective() {
+  // connect text-box to submit button and get my location
+  // need a button for locating me automactically
+  // gonna be using function expressions
+}
 
+const buttonSubmitHandler = function (e) {
+  // Not completed not working
+  e.preventDefault();
+  text.innerHTML = "";
+  let language = textBox.value;
+  console.log(language);
+  return console.log("hi");
+};
 
+buttonSubmitHandler();
 
-
-
-
-// SEARCH HISTORY FUNCTION (HISTORY PAGE)((LOCAL STORAGE))
-
-// function searchHistorySave(){
-//     save each user search "location name" --> saves to localStorage
-// }
-
+btnSubmit.addEventListener("click", buttonSubmitHandler);
