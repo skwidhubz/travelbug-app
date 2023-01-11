317e0fd103ea5aec97cb249d1f33a9d2ce57ca94
Dhruv-232 <dhruvsharma2004@outlook.com>
2023-01-11 12:09:24 +1100

    Js file (search history)

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,18 @@
-
-
-
-
-
-
+var historyEl = document.getElementById("history1")
+var historyList = document.getElementById("historyList")
+var textBox = document.getElementById("text-box")
+var submitButton = document.getElementById("submit-btn")
 
 // SEARCH HISTORY FUNCTION (HISTORY PAGE)((LOCAL STORAGE))
-
+function searchHistory(event) {
+    historyList.innerHTML = textBox.value
+    const li = document.createElement("li")
+    historyList.appendChild(li)
+    localStorage.setItem('value', textBox.value)
+    event.preventDefault()
+    // historyList.innerHTML = localStorage.getItem('value')
+}
+submitButton.addEventListener("click", searchHistory)
 // function searchHistorySave(){
 //     save each user search "location name" --> saves to localStorage
 // }
