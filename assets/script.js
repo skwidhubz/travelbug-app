4f154c9bd41a56cca77d13ebbb039e9b429a2900
byunn90 <kayhanturk90@Live.com>
2023-01-12 19:27:53 +1100

    added api to submit button working

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ const btnFind = document.getElementById("find-btn");
 const textBox = document.querySelector(".input");
 const historyEl = document.getElementById("history1");
 const historyList = document.getElementById("historyList");
+const myErrorBox = document.querySelector(".control");
 
 // Kayhan Objective Function
 function objective() {
@@ -14,7 +15,16 @@ function objective() {
 const areaText = function (e) {
   e.preventDefault();
   const language = textBox.value;
-  console.log(language);
+  if (language) {
+    streetAdd(language);
+  } else {
+    console.log("Please Type something");
+    const hMe = document.createElement("h1");
+    myErrorBox.appendChild(hMe);
+    hMe.classList.add("error-message");
+    hMe.innerText = "Error, Please type in a location";
+    hMe.style.color = "red";
+  }
 };
 
 const buttonSubmitHandler = function (e) {
@@ -36,16 +46,20 @@ function searchHistory() {
   historyList.appendChild(li);
 }
 
-const getLocation = function () {
-  const city = "paris";
-  const OpenApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=54f233828acf58994eefa05b9027dd89`;
+const getLocation = function (user) {
+  // const city = "paris";
+  const OpenApi = `https://api.openweathermap.org/data/2.5/weather?q=${user}&appid=54f233828acf58994eefa05b9027dd89`;
 
   fetch(OpenApi)
     .then((response) => response.json())
-    .then((response) => console.log(response));
+    .then((response) => {
+      const lat = response.coord.lat;
+      const lon = response.coord.lon;
+      if (response) {
+        locateMe(lat, lon);
+      }
+    });
 };
-
-getLocation();
 
 // Google API
 const locateMe = function (lat, lng) {
@@ -55,12 +69,30 @@ const locateMe = function (lat, lng) {
     .then((response) => response.json())
     .then((response) => console.log(response));
 };
-locateMe(-37.96944, 145.04806);
 
-btnSubmit.addEventListener("click", areaText);
-btnFind.addEventListener("click", buttonFindLocationHandler);
 // historyEl.addEventListener("click", searchHistory);
 
 // function searchHistorySave(){
 //     save each user search "location name" --> saves to localStorage
 // }
+const successCallback = (position) => {
+  console.log(position);
+};
+
+function getC() {
+  navigator.geolocation.getCurrentPosition(successCallback);
+}
+
+function streetAdd(address) {
+  const myAPI = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyBrNLhoIcFa9qPY_bPWF-qVPVF9qRS3dc8`;
+  fetch(myAPI)
+    .then((response) => response.json())
+    .then((response) => {
+      if (response) {
+        console.log(response.results[0].formatted_address);
+      }
+    });
+}
+
+btnSubmit.addEventListener("click", areaText);
+btnFind.addEventListener("click", buttonFindLocationHandler);
